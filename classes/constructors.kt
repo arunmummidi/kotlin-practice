db25a0a6f535f3988149dcf5ab5bcccdf39c5a77
Arun Mummidi <arunmummidi@gmail.com>
2023-05-03 22:24:40 +0530

    added parking lot stage 4

---
+++ classes/constructors.kt
@@ -0,0 +1,5 @@
+/*Write a class Pet that has three String properties declared in the primary constructor: name, animal, and owner.
+
+The default value for animal should be "cat".*/
+// write the Pet class here
+class Pet(var name: String, var animal: String = "cat", var owner: String)
