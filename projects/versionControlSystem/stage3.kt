a89fead6dd5e74d98483dc2b99a8fee5a4aa9ee3
arun <arunmummidi@gmail.com>
2023-06-14 02:37:32 +0530

    version control stage3 wip

--- projects/versionControlSystem/stage3.kt
+++ projects/versionControlSystem/stage3.kt
@@ -1,46 +1,74 @@
 import java.io.File
+import java.nio.file.Files
 import java.security.MessageDigest
 import java.util.*
 
+val md: MessageDigest = MessageDigest.getInstance("SHA-256")
 val separator: String? = File.separator
 val vcsRootDir = "vcs"
 val vcsCommits = vcsRootDir + separator + "commits"
 val vcsConfig = vcsRootDir + separator + "config.txt"
 val vcsIndex = vcsRootDir + separator + "index.txt"
 val vcsLog = vcsRootDir + separator + "log.txt"
+var currentCommitID = ""
+var stagedChanges = ""
 
 fun main(args: Array<String>) {
     createFileStructure()
     when(val input = args.firstOrNull()) {
-        null, "--help" -> printHelpPage()
+        "null", null, "--help" -> printHelpInfo()
         "config" -> {
             if(args.size > 1){
-                config(args[1].toString())
-            } else config()
+                writeConfig(args[1].toString())
+            } else showConfig()
         }
         "add" -> {
+            // Append file name to index file if add invoked with file name
             if(args.size > 1) {
-                appendFileToIndex(args[1].toString())
+                addFileToStaging(args[1].toString())
             } else {
-                showFiles()
+                // If add invoked with no arguments, show the added files
+                showStagedFiles()
             }
         }
         "log" -> showLog()
         "commit" -> if(args.size > 1) {
-            commit(args[1].toString())
+            stagedChanges = hasNewChanges()
+            if(hasNewChanges() == "changed") {
+                commit(args[1].toString())
+            } else println("Nothing to commit.")
+
         }else {
             println("Message was not passed.")
         }
         "checkout" -> println("Restore a file.")
-        "null" -> printHelpPage()
         else -> println("'$input' is not a SVCS command.")
     }
 }
 
+fun hasNewChanges(): String {
+    if(File(vcsLog).readText().isNotEmpty()){
+        val lastCommitDirectory = File(vcsLog).readText().substringAfter(' ').substringBefore('\n')
+        println(vcsCommits + separator + lastCommitDirectory)
+        val files = File(vcsCommits + separator + lastCommitDirectory).listFiles()
+
+        for(committedFile in files!!) {
+            for(sourceFile in File(vcsIndex).readLines()) {
+                if(sourceFile == committedFile.name){
+                    println("$sourceFile version exists")
+                    if(!md.digest(File(sourceFile).readBytes()).contentEquals(md.digest(committedFile.readBytes()))) {
+                        return "changed"
+                    }
+                }
+            }
+        }
+    }
+    return "not changed"
+}
 
 fun writeLog(commitId: String, message: String) {
     val authorName = File(vcsConfig).readText()
-    File(vcsLog).
+
     File(vcsLog).appendText("commit $commitId\n")
     File(vcsLog).appendText("Author: $authorName\n")
     File(vcsLog).appendText(message + "\n")
@@ -54,16 +82,15 @@ fun showLog() {
 
 fun commit(message: String) {
     val commitID = UUID.randomUUID()
-    val md = MessageDigest.getInstance("SHA-256")
 
     for(fileName in File(vcsIndex).readLines()) {
         val destinationFile = vcsCommits + separator + commitID + separator + fileName
-        if(File(destinationFile).exists()){
-            if(md.digest(File(fileName).readBytes()).contentEquals(md.digest(File(destinationFile).readBytes()))) {
-                println("Nothing to commit.")
-                break
-            }
-        }
+//        if(File(destinationFile).exists()){
+//            if(md.digest(File(fileName).readBytes()).contentEquals(md.digest(File(destinationFile).readBytes()))) {
+//                println("Nothing to commit.")
+//                break
+//            }
+//        }
 
         File(vcsCommits + separator + commitID).mkdir()
         File(fileName).copyTo(File(destinationFile))
@@ -71,6 +98,7 @@ fun commit(message: String) {
 
     println("Changes are committed.")
     writeLog(commitID.toString(), message)
+    currentCommitID = commitID.toString()
 }
 
 fun createFileStructure() {
@@ -86,7 +114,7 @@ fun createFileStructure() {
     if(!File(vcsLog).exists()) File(vcsLog).createNewFile()
 }
 
-fun appendFileToIndex(fileName: String) {
+fun addFileToStaging(fileName: String) {
     if(File(fileName).exists()) {
         if(File(vcsIndex).readText().contains(fileName)) {
             println("The file '$fileName' is tracked.")
@@ -102,7 +130,7 @@ fun appendFileToIndex(fileName: String) {
     }
 }
 
-fun printHelpPage() {
+fun printHelpInfo() {
     println("""
         These are SVCS commands:
         config     Get and set a username.
@@ -113,19 +141,19 @@ fun printHelpPage() {
     """.trimIndent())
 }
 
-fun config(userName: String) {
+fun writeConfig(userName: String) {
     println("The username is $userName.")
     val fileDescriptor = File(vcsConfig)
     fileDescriptor.writeText(userName)
 }
 
-fun config() {
+fun showConfig() {
     if(File(vcsConfig).readBytes().isEmpty()) {
         println("Please, tell me who you are.")
     } else println("The username is ${File(vcsConfig).readText().toString().trim('\n')}.")
 }
 
-fun showFiles() {
+fun showStagedFiles() {
     if(File(vcsIndex).readText().isEmpty()) {
         println("Add a file to the index.")
     } else {
