e5846c7b8cc6489ceea2ae55b72f74b0b8b49346
Arun Mummidi <arunmummidi@gmail.com>
2023-07-21 11:30:14 +0000

    sort descending

--- projects/andela/sortedPairs.kt
+++ projects/andela/sortedPairs.kt
@@ -2,29 +2,23 @@ import java.io.File
 
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
     val inputFile = args[0]
-    println("You have passed $inputFile as input")
-
-    val myMap = emptyMap<String, Int>().toMutableMap()
-
     val fileDescriptor = File(inputFile)
 
-    val myList = fileDescriptor.readText().replace(", ", "").replace(".", "").replace(";", "").split(" ").sorted()
+    val myMap = emptyMap<String, Int>().toMutableMap()
+    val wordList = fileDescriptor.readText().replace(", ", "").replace(".", "").replace(";", "").split(" ").sorted()
 
-    println(myList)
-
-    myList.forEach {
+    wordList.forEach {
         if(myMap.containsKey(it)) {
             myMap[it] = myMap[it]!! + 1
         } else myMap[it] = 1
     }
 
-    myMap.forEach{
+    val sortedMap = myMap.entries.sortedByDescending { it.value }.associate { it.toPair() }
+
+    sortedMap.forEach{
         println(it)
     }
 }
